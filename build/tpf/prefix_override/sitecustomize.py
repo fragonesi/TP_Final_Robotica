f5cb16c193f3891a676e97d6944b7ca8b598d040
Eliana <Elianaostro@gmail.com>
2026-07-01 09:27:50 -0300

    Agregar .gitignore y dejar de trackear build/install/log
    sistema3-landmarks se ramificó antes de que main/parte-A agregaran el
    .gitignore, así que build/, install/, log/, tpf/build/ y tpf/__pycache__
    quedaron commiteados por error. Se traen las mismas reglas ya validadas
    en parte-A (colcon, pycache, bags, gráficos generados).

--- build/tpf/prefix_override/sitecustomize.py
+++ build/tpf/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/alumno1/Downloads/TP_Final_Robotica-Localizing/install/tpf'
+    sys.prefix = sys.exec_prefix = '/home/elianaostro/Documents/Robotica/TP_Final_Robotica/install/tpf'
